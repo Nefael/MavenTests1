2de700f008161528a007d778ad4e0f692ceeda7d
Nefael <ronan_1994@hotmail.fr>
2016-09-07 10:56:27 +0200

    first commit add src and pom.xml

---
+++ src/main/java/testGroup/testArtifact/App.java
@@ -0,0 +1,18 @@
+package testGroup.testArtifact;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 * Ohohoh
+	 * Ahahah
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
